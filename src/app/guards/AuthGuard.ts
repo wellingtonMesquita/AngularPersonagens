9f679df6dfa5b4b85f05351d8b53c2bf1c47ca30
wellingtonMesquita <wellington88tads@gmail.com>
2018-08-20 15:10:09 -0300

    fred

--- src/app/guards/AuthGuard.ts
+++ src/app/guards/AuthGuard.ts
@@ -1,21 +1,20 @@
-﻿import { Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+
+
 
 
 @Injectable({ providedIn: 'root' })
 export class AuthGuard  {
-
     constructor(private router: Router) { }
-
     //Retorna true ou false caso o token exista. 
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
         if (localStorage.getItem('token')) {
             // logged in so return true
             return true;
         }
-
         // not logged in so redirect to login page with the return url
-        this.router.navigate(['/login'], { queryParams: { returnUrl: state.url }});
+        this.router.navigate(['/login'], { queryParams: { returnUrl: state.url } });
         return false;
     }
 }

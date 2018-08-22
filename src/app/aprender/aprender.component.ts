d048d30d99ba09fd278fe0e923fbb9874fd26733
wellingtonMesquita <wellington88tads@gmail.com>
2018-08-22 10:31:08 -0300

    mais um

--- src/app/aprender/aprender.component.ts
+++ src/app/aprender/aprender.component.ts
@@ -7,9 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AprenderComponent implements OnInit {
 
+
+
+  isVariavel:boolean;
+  umArray = ["Arroz","Feijao","Carne","Salada"];
   constructor() { }
 
   ngOnInit() {
-  }
+    this.isVariavel = false;
 
+  }
+ 
+ mudar(){
+    if(this.isVariavel){
+        this.isVariavel = false;
+    }else{
+        this.isVariavel = true;
+    }
+ }
+
+ alerta(item){
+    alert(item);
+ }
+
+
+ verEvento(event){
+    console.log(event.MouseEvent.type);
+ }
 }
